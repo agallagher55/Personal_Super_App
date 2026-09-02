4a0d35f92a1e76ce2c903394020359d048a35a6a
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2026-09-02 13:15:53 -0300

    Reorder commands in start-server.bat

--- backend/start-server.bat
+++ backend/start-server.bat
@@ -1,4 +1,4 @@
 @echo off
-"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" server.py
 set FITNESS_OWNER_EMAIL=agallagher55@gmail.com 
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" server.py
 pause
